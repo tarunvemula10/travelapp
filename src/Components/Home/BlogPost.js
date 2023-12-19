ada15f5273d11139320a867cf11eac7e721428c0
tharunvemula10 <tarun.vemula2445@gmail.com>
2023-12-19 20:31:36 +0530

    added blogs section

---
+++ src/Components/Home/BlogPost.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import headImage1 from './Images/blog-image-head-1.jpg'
+import headImage2 from './Images/blog-image-head-2.jpg'
+import cardImage1 from './Images/blog-image-card-1.jpg'
+import cardImage2 from './Images/blog-image-card-2.jpg'
+import cardImage3 from './Images/blog-image-card-3.jpg'
+
+export default function BlogPost() {
+     return (
+     <section className='blog-post mt-5'>
+          <div className="container text-center p-3 heading">
+               <p className='text-secondary'><small>OUR LATEST BLOG POSTS</small></p>
+               <h3><b>FROM THE RESOURCE CENTER</b></h3>
+          </div>
+          <div className="container">
+               <div className="row">
+                    <div className="col-sm pt-3">
+                         <div className="card text-bg-dark">
+                              <img src={headImage1} className="card-img img-fluid" alt="..."/>
+                              <div className="card-img-overlay">
+                                   <h5 className="card-title">The Tokyo Nights</h5>
+                                   <p className="card-text">Explore the beauty of tokyo in nights</p>
+                                   <p className="card-text"><small className='bg-info rounded py-1 px-2 text-dark'>Popular</small></p>
+                              </div>
+                         </div>
+                    </div>
+                    <div className="col-sm pt-3">
+                         <div className="card text-bg-dark">
+                              <img src={headImage2} className="card-img img-fluid" alt="..."/>
+                              <div className="card-img-overlay">
+                                   <h5 className="card-title">Incredible India</h5>
+                                   <p className="card-text">Unity in Diversity</p>
+                                   <small className='bg-info rounded py-1 px-2 text-dark'>Popular</small>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+
+               <div className="row mt-2">
+                    <div className="col-sm p-2">
+                         <div class="card">
+                              <img src={cardImage1} class="card-img-top" alt="..." height='250px' width='100%'/>
+                              <div class="card-body p-4">
+                                   <h5 class="card-title"><b>Surfing in maldivies</b></h5>
+                                   <p class="card-text">Unveil the secrets of the Indian Ocean's gentle giants as we explore the exhilarating world of surfing in the Maldives.</p>
+                                   <p class="card-text"><small class="text-body-secondary">Suggested - Diaries of Maldivies</small></p>
+                              </div>
+                         </div>
+                    </div>
+                    <div className="col-sm p-2">
+                         <div class="card">
+                              <img src={cardImage2} class="card-img-top" alt="..." height='250px' width='100%'/>
+                              <div class="card-body p-4">
+                                   <h5 class="card-title"><b>Beauty of God's own country - Kerala</b></h5>
+                                   <p class="card-text">Embark on a visual odyssey through the enchanting landscapes of Kerala, where nature weaves its magic in every hue and contour.</p>
+                                   <p class="card-text"><small class="text-body-secondary">Suggested - Incredible India</small></p>
+                              </div>
+                         </div>
+                    </div>
+                    <div className="col-sm p-2">
+                         <div class="card">
+                              <img src={cardImage3} class="card-img-top" alt="..." height='250px' width='100%'/>
+                              <div class="card-body p-4">
+                                   <h5 class="card-title"><b>Enchanting Kashmir</b></h5>
+                                   <p class="card-text">Immerse yourself in the ethereal beauty of snow-capped peaks as we explore the Himalayan wonders of Gulmarg and Pahalgam.</p>
+                                   <p class="card-text"><small class="text-body-secondary">Suggested - Incredible India</small></p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </section>
+     )
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,38 +2,37 @@ import React from 'react'
 import Intro from './Intro';
 import Whyus from './Whyus';
 import Coversection from './Coversection';
+// import Gallery from './Gallery';
 import coverImage1 from './Images/cover-image-1.jpg';
 import coverImage2 from './Images/cover-image-2.jpg';
 import coverImage3 from './Images/cover-image-3.jpg';
+import BlogPost from './BlogPost';
 
 export default function Home() {
-     return (
-     <>
-          <Intro/>
-          <Whyus/>
-          <Coversection
-               backgroundColor = 'bg-secondary-subtle'
-               imageOrder = 'order-sm-1'
-               image = {coverImage1}
-               descriptionOrder = 'order-sm-2'
-               heading = {['Plan Your Adventure', <br/>, 'with us']}
-               description = {`Embark on a journey of discovery with our unparalleled traveling site! 
+     const coverSectionItems = [
+          {
+               backgroundColor:'bg-secondary-subtle',
+               imageOrder: 'order-sm-1',
+               image: coverImage1,
+               descriptionOrder: 'order-sm-2',
+               heading: ['Plan Your Adventure', <br/>, 'with us'],
+               description: `Embark on a journey of discovery with our unparalleled traveling site! 
                Immerse yourself in a world of wanderlust as we curate unforgettable experiences, 
                from hidden gems to iconic landmarks. Navigate through a treasure trove of destination guides, 
                travel tips, and captivating narratives that will fuel your adventurous spirit. 
                Whether you crave the thrill of exotic landscapes, cultural escapades, 
                or simply the joy of a relaxing getaway, our traveling site is your compass 
                to a world full of possibilities. Unpack inspiration, explore the extraordinary, 
-               and let your wanderlust take flight!`}
-               buttonTitle = 'Plan your Journey'
-          />
-          <Coversection
-               backgroundColor = 'bg-light'
-               imageOrder = 'order-sm-2'
-               image = {coverImage2}
-               descriptionOrder = 'order-sm-1'
-               heading = {['Explore the world', <br/>, 'through Activities']}
-               description = {`Dive into a world of adrenaline-fueled excitement with our collection of thrilling 
+               and let your wanderlust take flight!`,
+               buttonTitle: 'Plan your Journey'
+          },
+          {
+               backgroundColor:'bg-transparent',
+               imageOrder: 'order-sm-2',
+               image: coverImage2,
+               descriptionOrder: 'order-sm-1',
+               heading: ['Explore the world', <br/>, 'through Activities'],
+               description: `Dive into a world of adrenaline-fueled excitement with our collection of thrilling 
                activities! Unleash your inner daredevil as we guide you through heart-pounding 
                adventures that redefine the meaning of exhilaration. From soaring heights to 
                uncharted depths, immerse yourself in an array of pulse-quickening experiences. 
@@ -41,16 +40,16 @@ export default function Home() {
                rafting expedition, or exploring mystical caves, our thrilling activities 
                guarantee an unforgettable journey for the fearless explorer in you. 
                Get ready to elevate your travel experience and make every moment 
-               an adventure to remember!`}
-               buttonTitle = 'Explore the Adventures'
-          />
-          <Coversection
-               backgroundColor = 'bg-secondary-subtle'
-               imageOrder = 'order-sm-1'
-               image = {coverImage3}
-               descriptionOrder = 'order-sm-2'
-               heading = {['Get together and', <br/>, 'make Memories']}
-               description = {`Create timeless moments that linger in the heart with our travel 
+               an adventure to remember!`,
+               buttonTitle: 'Explore the Adventures'
+          },
+          {
+               backgroundColor:'bg-secondary-subtle',
+               imageOrder: 'order-sm-1',
+               image: coverImage3,
+               descriptionOrder: 'order-sm-2',
+               heading: ['Get together and', <br/>, 'make Memories'],
+               description: `Create timeless moments that linger in the heart with our travel 
                experiences designed for memory-making. Immerse yourself in a symphony 
                of unforgettable adventures, captivating landscapes, and cultural 
                treasures. From sun-kissed beaches to ancient wonders, every destination 
@@ -59,9 +58,27 @@ export default function Home() {
                away, or the flavors that linger on your palate, our traveling site 
                is your gateway to a tapestry of moments waiting to be woven into the 
                story of a lifetime. Pack your bags and let's turn your 
-               journey into a masterpiece of memories!`}
-               buttonTitle = 'Get started'
-          />
+               journey into a masterpiece of memories!`,
+               buttonTitle: 'Get started'
+          }
+     ]
+     return (
+     <>
+          <Intro/>
+          <Whyus/>
+          {coverSectionItems.map((element)=> {
+               return <Coversection
+                    backgroundColor = {element.backgroundColor}
+                    imageOrder = {element.imageOrder}
+                    image = {element.image}
+                    descriptionOrder = {element.descriptionOrder}
+                    heading = {element.heading}
+                    description = {element.description}
+                    buttonTitle = {element.buttonTitle}
+               />
+          })}
+          <BlogPost/>
+          {/* <Gallery/> */}
      </>
      )
 }
